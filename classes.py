a60bf80c5330b0506046d01c40c746c66c2f7916
dimancik79 <dimanchik.dmitri@yandex.ru>
2023-10-12 16:23:35 +0700

    12/10-16:23-processed

--- classes.py
+++ classes.py
@@ -1,12 +1,9 @@
-from tkinter.ttk import Treeview
-from PIL import Image
-from customtkinter import *
-from tkinter.filedialog import askopenfilenames
-from tkinter import ttk, font
-from models import PlayList
+import os
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
+from PyQt5.QtWidgets import QMainWindow
 from tinytag import TinyTag
-import threading
-import time
+from models import PlayList
+
 import pygame
 
 
@@ -23,13 +20,13 @@ def get_time(duration):
     return f"{hour:}:{minute}:{second[0:] if len(second) < 3 else second[1:]}"
 
 
-class MainClass:
-    def __init__(self, root: list) -> None:
-        self.playlist = {}
-        self.root = root
+class MainClass(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.play_list = {}
         self.pause = False
         self.start = False
-        self.x_pos = 2
         self.duration = None
         self.duretion_sec = None
         self.duratoin_old = None
@@ -37,106 +34,29 @@ class MainClass:
         self.song_id_old = None
         self.total_songs = 0
         self.wait = 0
+
+        uic.loadUi("player.ui", self)
         
-        # кнопка play
-        img = CTkImage(light_image=Image.open("IMG/play.ico"), size=(24, 24))
-        self.btn_play = CTkButton(self.root[1], image=img, text="", width=24, height=24,
-                                  command=self.press_play_button)
-        self.btn_play.place_configure(x=self.x_pos, y=2)
-
-        # кнопка stop
-        img = CTkImage(light_image=Image.open("IMG/stop.ico"), size=(24, 24))
-        self.btn_stop = CTkButton(self.root[0], image=img, text="", width=24, height=24,
-                                  command=self.press_stop_button)
-        self.btn_stop.place_configure(x=260, y=112)
-
-        # кнопка next
-        img = CTkImage(light_image=Image.open("IMG/next.ico"), size=(24, 24))
-        self.btn_next = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_next.place_configure(x=300, y=112)
-
-        # бегунок slider
-        self.slider_volume = CTkSlider(self.root[2], from_=0, to=100, height=12, width=95)
-        self.slider_volume.place_configure(x=5, y=17)
-
-        # кнопка add
-        img = CTkImage(light_image=Image.open("IMG/add.ico"), size=(24, 24))
-        self.btn_add = CTkButton(self.root[0], image=img, text="", width=24, height=24, command=self.thred_do_file_add)
-        self.btn_add.place_configure(x=5, y=461)
-
-        # кнопка delete
-        img = CTkImage(light_image=Image.open("IMG/delete.ico"), size=(24, 24))
-        self.btn_delete = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_delete.place_configure(x=45, y=461)
-
-        # кнопка clear
-        img = CTkImage(light_image=Image.open("IMG/clear.ico"), size=(24, 24))
-        self.btn_clear = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_clear.place_configure(x=85, y=461)
-
-        # кнопка exit
-        img = CTkImage(light_image=Image.open("IMG/exit.ico"), size=(24, 24))
-        self.btn_exit = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_exit.place_configure(x=407, y=461)
         
-         # кнопка up
-        img = CTkImage(light_image=Image.open("IMG/up.ico"), size=(24, 24))
-        self.btn_up = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_up.place_configure(x=200, y=461)
-
-        # кнопка down
-        img = CTkImage(light_image=Image.open("IMG/down.ico"), size=(24, 24))
-        self.btn_down = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_down.place_configure(x=240, y=461)
+        self.playlist.setHeaderLabels(['Song','Time'])
         
-        # кнопка pin
-        img = CTkImage(light_image=Image.open("IMG/pin.ico"), size=(24, 24))
-        self.btn_pin = CTkButton(self.root[0], image=img, text="", width=24, height=24)
-        self.btn_pin.place_configure(x=280, y=461)
-        
-        # список tree
-        style = ttk.Style()
-        style.theme_use("clam")
-        font.nametofont('TkHeadingFont').configure(family="Calibri", size=10)
-        style.configure(".", font=('Calibri', 9, "roman"), foreground="black")
-        columns = ("name_song", "time_song")
-        self.tree = Treeview(self.root[3], name="tree", columns=columns, show="headings", height=13, padding=2)
-        ttk.Style().configure("Treeview", background="black", foreground="white", fieldbackground="black")
-        self.tree.pack(side=LEFT)
-        self.tree.heading("name_song", text="Song`s name", anchor=W)
-        self.tree.heading("time_song", text="Time", anchor=W)
-        self.tree.column("#1", stretch=NO, width=348, anchor=W)
-        self.tree.column("#2", stretch=NO, width=70, anchor=E)
-        self.tree.bind("<Return>", self.bind_tree_change_song)
-        self.tree.bind("<Double-ButtonPress-1>", self.bind_tree_change_song)
-        scrollbar = ttk.Scrollbar(self.root[3], orient=VERTICAL, command=self.tree.yview)
-        self.tree["yscrollcommand"] = scrollbar.set
-        scrollbar.place(y=0, x=374)
-        scrollbar.pack(side="right", fill="y")
-        self.update_playlist()
+        self.add.clicked.connect(self.file_add)
+        self.play.clicked.connect(self.press_play_button)
+        self.stop.clicked.connect(self.press_stop_button)
+    
         
     def bind_tree_change_song(self, event):
         self.press_stop_button()
         self.press_play_button()
 
-    def thred_do_file_add(self):
-        for thred in threading.enumerate():
-                if "file_add" in str(thred):
-                    return
-        else:
-            threading.Thread(target=self.file_add, args=(), daemon=True).start()
-
     def file_add(self):
-        allowed_extensions = ('.mp3', '.wav', ".mp4", '.avi')
         song_files = []
-        songs = list(askopenfilenames())
-        for element in songs:
-            for ext in allowed_extensions:
-                if element.find(ext) != -1:
-                    song_files += (element, )
-                    break
-        if song_files == "":
+        songs = QtWidgets.QFileDialog.getOpenFileNames(filter="*.mp3 *.wav")[0]
+        if not songs:
             return
+        
+        for element in songs:
+            song_files += (element, )
         
         for song_file in song_files:
             song = TinyTag.get(song_file)
@@ -150,60 +70,61 @@ class MainClass:
     def press_stop_button(self):
         self.start = False
         self.pause = False
-        self.x_pos = 2
-        self.btn_play.configure(image=CTkImage(light_image=Image.open("IMG/play.ico"), size=(24, 24)))
-        self.btn_play.place_configure(x=self.x_pos)
-        self.btn_play.update()
+        # self.btn_play.configure(image=CTkImage(light_image=Image.open("IMG/play.ico"), size=(24, 24)))
+        # self.btn_play.place_configure(x=self.x_pos)
+        # self.btn_play.update()
         pygame.mixer.music.stop()
         
     def press_play_button(self):
-        if not self.start:
-            self.start = True
-            path = self.playlist[int(self.tree.selection()[0])][1]
-            self.duration = self.playlist[int(self.tree.selection()[0])][2]
-            self.duration_sec = self.playlist[int(self.tree.selection()[0])][3]
-            self.wait = int((self.duration_sec * 100 / 250) / 10)
-            print(self.wait)
+        # if not self.start:
+            # self.start = True
+            # path = self.playlist[int(self.tree.selection()[0])][1]
+            # self.duration = self.playlist[int(self.tree.selection()[0])][2]
+            # self.duration_sec = self.playlist[int(self.tree.selection()[0])][3]
+            # self.wait = self.duration_sec / 250
+            # print(self.wait)
             
-            if self.song_id_old != None:
-                self.tree.tag_configure('white', foreground='white')
-                self.tree.item(self.song_id_old, tag='white')
-                self.tree.set(self.song_id, 1, self.duratoin_old)
+            # if self.song_id_old != None:
+            #     self.tree.tag_configure('white', foreground='white')
+            #     self.tree.item(self.song_id_old, tag='white')
+            #     self.tree.set(self.song_id, 1, self.duratoin_old)
             
-            self.song_id = str(self.tree.selection()[0])
-            self.song_id_old = self.song_id
-            self.duratoin_old = self.playlist[int(self.tree.selection()[0])][2]     
-            self.tree.tag_configure('pink', foreground='pink')
-            self.tree.item(self.song_id, tag='pink')
-            pygame.mixer.music.load(path)
-            pygame.mixer.music.play(loops=0)
-            for thred in threading.enumerate():
-                if "play_music" in str(thred):
-                    return
-            else:
-                threading.Thread(target=self.play_music, args=(), daemon=True).start()
-                return
+            # self.song_id = str(self.tree.selection()[0])
+            # self.song_id_old = self.song_id
+            # self.duratoin_old = self.playlist[int(self.tree.selection()[0])][2]     
+            # self.tree.tag_configure('pink', foreground='pink')
+            # self.tree.item(self.song_id, tag='pink')
+            # pygame.mixer.music.load(path)
+            # pygame.mixer.music.play(loops=0)
+            # for thred in threading.enumerate():
+            #     if "play_music" in str(thred):
+            #         return
+            # else:
+            #     threading.Thread(target=self.play_music, args=(), daemon=True).start()
+            #     return
         
-        if self.start and not self.pause:
-            self.btn_play.configure(image=CTkImage(light_image=Image.open("IMG/pause.ico"), size=(24, 24)))
+        if not self.pause:
+            self.play.setIcon(QtGui.QIcon("IMG/pause.ico"))
+            self.play.setIconSize(QtCore.QSize(28,28))
             self.pause = True
-            pygame.mixer.music.pause()
+            # pygame.mixer.music.pause()
         else:
-            self.btn_play.configure(image=CTkImage(light_image=Image.open("IMG/play.ico"), size=(24, 24)))
+            self.play.setIcon(QtGui.QIcon("IMG/play.ico"))
+            self.play.setIconSize(QtCore.QSize(28,28))
             self.pause = False
-            pygame.mixer.music.unpause()
-            threading.Thread(target=self.play_music, args=(), daemon=True).start()
+            # pygame.mixer.music.unpause()
+            # threading.Thread(target=self.play_music, args=(), daemon=True).start()
     
     def play_music(self):
         while get_time(pygame.mixer.music.get_pos()) != self.duration:
             if self.pause or not self.start:
                 return
             pygame.mixer.music.set_volume(self.slider_volume.get() / 100)
-            self.tree.set(self.song_id, 1, get_time(pygame.mixer.music.get_pos()))
-            if (pygame.mixer.music.get_pos() // self.wait) == 0:
-                self.x_pos += 1
-                self.btn_play.place_configure(x=self.x_pos)
-                self.btn_play.update()
+            # self.tree.set(self.song_id, 1, get_time(pygame.mixer.music.get_pos()))
+            # time.sleep(self.wait)
+            # self.x_pos += 1
+            self.btn_play.place_configure(x=self.x_pos)
+            self.btn_play.update()
         
         pygame.mixer.music.stop()
         
@@ -216,16 +137,3 @@ class MainClass:
                 self.tree.insert("", END, iid=str(row.id), values=(row.song_name, row.duration))
             self.playlist[row.id] = [row.song_name, row.song_path, row.duration, row.duration_sec]
         
-    def play_time_duration():
-        # 
-        #     run_string_news()
-        #     if get_flags("addplay") == 1:
-        #         t
-        #     pygame.mixer.music.set_volume(volume_scale.get() / 100)
-        #     progress.config(value=pygame.mixer.music.get_pos())
-        #     progress.update()
-        #     label_time.config(text=get_time(pygame.mixer.music.get_pos()), fg="black")
-        #     label_time.update()
-        # set_flags('music_play', 0)
-        # next_song()
-        pass
--- main.py
+++ main.py
@@ -1,37 +1,16 @@
-from customtkinter import *
+import sys
+from PyQt5.QtWidgets import QApplication
+
 from os import path
 from models import PlayList
 from classes import MainClass
 
 
 def main():
-    color = ["cornsilk4", "black"]
-    root = CTk()
-    root.title("MUSIC PLAYER")
-    root.geometry("450x500")
-    root.resizable(False, False)
-    root.configure(fg_color=color)
-    
-    set_default_color_theme("green")
-
-    frame_main = CTkFrame(root, height=100, width=440, border_width=1, fg_color=color[1], border_color="white")
-    frame_main.place_configure(x=5, y=5)
-
-    frame_progress = CTkFrame(root, height=35, width=250, border_width=1, border_color="white", fg_color=color[1])
-    frame_progress.place_configure(x=5, y=110)
-
-    frame_volume = CTkFrame(root, height=35, width=103, border_width=1, border_color="white", fg_color=color[1])
-    frame_volume.place_configure(x=342, y=110)
-    frame_volume.place()
-    lbl = CTkLabel(frame_volume, text="volume", font=("Calibri", 9), height=8, fg_color="white", width=93, text_color="black")
-    lbl.place_configure(x=5, y=4)
-
-    frame_list = CTkFrame(root, height=300, width=440, border_width=1, border_color="black", fg_color=color[1])
-    frame_list.place_configure(x=5, y=150)
-
-    all_widget = MainClass(root=[root, frame_progress, frame_volume, frame_list])
-
-    root.mainloop()
+    app = QApplication(sys.argv)
+    mainwindow = MainClass()
+    mainwindow.show()
+    sys.exit(app.exec())
     
 
 if __name__ == "__main__":
